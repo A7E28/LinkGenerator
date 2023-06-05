4a86c7e10eecedd20b9335de6259b92ea8c7aa29
MD Abdul Aziz <110674887+HYPERNNOVA@users.noreply.github.com>
2023-06-06 04:08:10 +0600

    Add files via upload

---
+++ LinkGenerator.py
@@ -0,0 +1,36 @@
+import logging
+from telegram import Update, ChatInviteLink
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+
+
+logging.basicConfig(level=logging.INFO)
+updater = Updater("Bot_Token")
+
+
+
+def get_link(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    try:
+        limit = int(context.args[0])
+    except (IndexError, ValueError):
+        update.message.reply_text("Usage: /get link <number_of_uses>")
+        return
+
+    if limit < 1:
+        update.message.reply_text("The number of uses should be at least 1.")
+        return
+
+    try:
+        invite_link = context.bot.create_chat_invite_link(chat_id, member_limit=limit)
+        update.message.reply_text(f"Here's your invite link: {invite_link.invite_link}")
+    except Exception as e:
+        update.message.reply_text("An error occurred while generating the invite link.")
+        logging.error(e)
+
+
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler("get", get_link, pass_args=True))
+updater.start_polling()
+updater.idle()
