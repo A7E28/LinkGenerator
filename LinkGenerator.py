0780a7c13832dbfa7d9f9201de5a43857cd42fc1
MD Abdul Aziz <110674887+HYPERNNOVA@users.noreply.github.com>
2023-06-06 23:59:30 +0600

    Introducing V2.0 of Link Generator
    on version 2.0 changes are:
    # user can now set time limit for link
    # new command added /migrateid for migrating your existing account with new/other account 
    # /migrateid link has 5min time limit
    
    About migrateid command: If a user type /migrateid <userid> the bot will ban the user(who executed migrate cmd) and will send a new invite link to the <userid> account

--- LinkGenerator.py
+++ LinkGenerator.py
@@ -1,11 +1,13 @@
 import logging
-from telegram import Update, ChatInviteLink
+import time
+from datetime import datetime, timedelta
+from telegram import Update, ChatMember
 from telegram.ext import Updater, CommandHandler, CallbackContext, Filters
 
 logging.basicConfig(level=logging.INFO)
 updater = Updater("Bot_Token")
 
-VERSION = "V1.0.18"
+VERSION = "V2.0"
 
 def get_link(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -16,17 +18,24 @@ def get_link(update: Update, context: CallbackContext):
 
     try:
         limit = int(context.args[0])
+        duration = int(context.args[1])
     except (IndexError, ValueError):
-        update.message.reply_text("Usage: /getlink <number_of_uses>")
+        update.message.reply_text("Usage: /getlink <number_of_uses> <duration_in_minutes>")
         return
 
     if limit < 1:
         update.message.reply_text("The number of uses should be at least 1.")
         return
 
+    if duration < 1:
+        update.message.reply_text("The duration should be at least 1 minute.")
+        return
+
     try:
-        invite_link = context.bot.create_chat_invite_link(chat_id, member_limit=limit)
+        expire_date = datetime.utcnow() + timedelta(minutes=duration)
+        invite_link = context.bot.create_chat_invite_link(chat_id, member_limit=limit, expire_date=expire_date)
         context.chat_data['invite_link'] = invite_link.invite_link
+        context.chat_data['expire_date'] = expire_date.timestamp()
         update.message.reply_text(f"Here's your invite link: {invite_link.invite_link}")
     except Exception as e:
         update.message.reply_text("An error occurred while generating the invite link.")
@@ -50,12 +59,47 @@ def revoke_link(update: Update, context: CallbackContext):
         logging.error(e)
 
 
+def migrateid(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    user_id = update.effective_user.id
+
+    try:
+        target_user_id = int(context.args[0])
+    except (IndexError, ValueError):
+        update.message.reply_text("Usage: /migrateid <user_id>")
+        return
+
+    if user_id == target_user_id:
+        update.message.reply_text("You cannot migrate yourself.")
+        return
+
+    try:
+        context.bot.ban_chat_member(chat_id, user_id)
+        expire_date = datetime.utcnow() + timedelta(minutes=1)
+        invite_link = context.bot.create_chat_invite_link(chat_id, member_limit=1, expire_date=expire_date)
+        context.bot.send_message(target_user_id, f"Migrate link: {invite_link.invite_link}")
+    except Exception as e:
+        update.message.reply_text("An error occurred while migrating the user.")
+        logging.error(e)
+
+def check_expired_links(context: CallbackContext):
+    for chat_id, chat_data in context.chat_data.items():
+        if 'expire_date' in chat_data and chat_data['expire_date'] <= time.time():
+            try:
+                context.bot.revoke_chat_invite_link(chat_id, chat_data['invite_link'])
+                del chat_data['invite_link']
+                del chat_data['expire_date']
+            except Exception as e:
+                logging.error(e)
+
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(f"Hey there, I'm alive! This is Link Generator {VERSION}")
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("getlink", get_link, pass_args=True, filters=Filters.chat_type.groups))
 dispatcher.add_handler(CommandHandler("revoke", revoke_link, filters=Filters.chat_type.groups))
+dispatcher.add_handler(CommandHandler("migrateid", migrateid, pass_args=True, filters=Filters.chat_type.groups))
 dispatcher.add_handler(CommandHandler("start", start))
+updater.job_queue.run_repeating(check_expired_links, interval=60, first=0)
 updater.start_polling()
 updater.idle()
